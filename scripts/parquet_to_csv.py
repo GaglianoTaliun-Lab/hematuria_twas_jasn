0474bcd86d737b5781e45d314a31b9dc18a6f085
ape4fld <fridalona@gmail.com>
2023-01-12 14:41:56 -0500

    removed folder from repository, keeping it locally

--- scripts/parquet_to_csv.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import pyarrow
-
-HOMEPATH="/home/fridald4/projects/def-gsarah/fridald4/renal_genetics_project/MR_analysis/"
-
-df = pd.read_parquet(HOMEPATH + 'Nerve_Tibial.v8.EUR.allpairs.chr3.parquet')
-df.to_csv(HOMEPATH + 'Nerve_Tibial.v8.EUR.allpairs.chr3.csv')
